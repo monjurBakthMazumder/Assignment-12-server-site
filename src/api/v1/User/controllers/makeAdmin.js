cd8f8b78adc65f76809a651d884252f800371089
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-11-25 18:27:03 +0600

    create api for make admin and premium user

---
+++ src/api/v1/User/controllers/makeAdmin.js
@@ -0,0 +1,13 @@
+const User = require("../../../../models/user");
+
+const makeAdmin = async (req, res) => {
+  const user = req.body;
+  const email = req.body.email
+  console.log(email);
+  const result = await User.findOneAndUpdate({ email: email }, user, {
+    new: true,
+  });
+  res.send(result);
+};
+
+module.exports = makeAdmin;
--- src/routes/v1/user/index.js
+++ src/routes/v1/user/index.js
@@ -1,5 +1,6 @@
 const allUser = require('../../../api/v1/User/controllers/allUser')
 const createUser = require('../../../api/v1/User/controllers/createUser')
+const makeAdmin = require('../../../api/v1/User/controllers/makeAdmin')
 const premiumUser = require('../../../api/v1/User/controllers/premiumUser')
 
 const router = require('express').Router()
@@ -7,5 +8,7 @@ const router = require('express').Router()
 router.get('/users/premium/:email', premiumUser)
 router.get('/users', allUser)
 router.post('/users', createUser)
+router.put('/users/make-admin', makeAdmin)
+router.put('/users/make-premium-user', makeAdmin)
 
 module.exports = router
