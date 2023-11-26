5e17e3a90dfe2037a0d40c63afc13a33e650cb0d
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-11-27 00:32:22 +0600

    create filter funtionality api for biodata

--- src/api/v1/BioData/controllers/findAll.js
+++ src/api/v1/BioData/controllers/findAll.js
@@ -1,9 +1,47 @@
-const findAllBioData = require("../../../../lib/BioData/FindAllBioData")
-
+const BioData = require("../../../../models/Biodata");
 
 const findAll = async (req, res) => {
-    const allBioData =await findAllBioData()
-    res.send(allBioData)
-}
+  const filter = {};
+  const ageTo = req.query?.ageTo !== "undefined" ? req.query?.ageTo : undefined;
+  const ageFrom =
+    req.query?.ageFrom !== "undefined" ? req.query?.ageFrom : undefined;
+  const gender =
+    req.query?.gender !== "undefined" ? req.query?.gender : undefined;
+  const permanentDivision =
+    req.query?.permanentDivision !== "undefined"
+      ? req.query?.permanentDivision
+      : undefined;
+  const presentDivision =
+    req.query?.presentDivision !== "undefined"
+      ? req.query?.presentDivision
+      : undefined;
 
-module.exports = findAll
+  if (ageTo && ageFrom) {
+    filter.age = { $gte: ageFrom, $lte: ageTo };
+  } else if (ageTo) {
+    filter.age = { $lte: ageTo };
+  } else if (ageFrom) {
+    filter.age = { $gte: ageFrom };
+  }
+
+  if (gender) {
+    filter.gender = gender;
+  }
+
+  if (permanentDivision) {
+    filter.permanentDivision = permanentDivision;
+  }
+  if (presentDivision) {
+    filter.presentDivision = presentDivision;
+  }
+  console.log("filter", filter);
+  console.log("filterOption", req.query);
+  try {
+    const allBioData = await BioData.find(filter);
+    res.send(allBioData);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = findAll;
