9392d8a8cd1183d653338d79f0205cdc5b6e59e3
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-11-26 18:53:06 +0600

    update single datails api

--- src/api/v1/BioData/controllers/singleBiodata.js
+++ src/api/v1/BioData/controllers/singleBiodata.js
@@ -1,15 +1,21 @@
-const BioData = require("../../../../models/Biodata")
+const BioData = require("../../../../models/Biodata");
+const Payment = require("../../../../models/Payment");
 
-
-const singleBiodata = async(req, res) => {
-    try {
-        const id = req.params.id
-        const result = await BioData.findOne({_id : id})
-        res.send(result)
-    } catch (error) {
-        console.log(error.message);
+const singleBiodata = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const email = req.query.email;
+    console.log("email", email);
+    let requested = ""; 
+    const exiting = await Payment.findOne({bioData_Id : id, selfEmail : email})
+    if(exiting) {
+        requested = exiting?.status
     }
-   
-}
+    const result = await BioData.findOne({ _id: id });
+    res.send({result, requested});
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-module.exports = singleBiodata
+module.exports = singleBiodata;
