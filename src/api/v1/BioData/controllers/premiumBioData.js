ebc1233dd977aa94ba692f9ef9dd91fb63de0727
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-11-29 14:12:49 +0600

    update

--- src/api/v1/BioData/controllers/premiumBioData.js
+++ src/api/v1/BioData/controllers/premiumBioData.js
@@ -1,7 +1,7 @@
 const BioData = require("../../../../models/BioData");
 
 const premiumBioData = async (req, res) => {
-    const result = await BioData.find({premium : true}).limit(6)
+    const result = await BioData.find({premium : true}).limit(6).sort({ age: -1 });
     res.send(result)
 };
 
