b4e62783005b673a99f0cc532b342469a3441d7d
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-11-29 10:53:23 +0600

    add limit in similar biodata

--- src/api/v1/BioData/controllers/similarBiodata.js
+++ src/api/v1/BioData/controllers/similarBiodata.js
@@ -4,7 +4,7 @@ const BioData = require("../../../../models/BioData");
 const similarBiodata = async ( req, res) => {
     try {
         const gender = req.params.gender
-    const result = await BioData.find({ gender: gender })
+    const result = await BioData.find({ gender: gender }).limit(10)
     res.send(result);
     } catch (error) {
         console.log(error.message);
