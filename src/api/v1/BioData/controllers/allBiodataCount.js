49a1953b9b2215584fc29b9eb7802e1dc38ea35e
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-11-30 19:15:21 +0600

    update count all biodata api

--- src/api/v1/BioData/controllers/allBiodataCount.js
+++ src/api/v1/BioData/controllers/allBiodataCount.js
@@ -2,8 +2,41 @@ const BioData = require("../../../../models/BioData");
 
 const allBiodataCount = async (req, res) => {
   try {
+    const filter = {};
+    const ageTo =
+      req.query?.ageTo !== "undefined" ? req.query?.ageTo : undefined;
+    const ageFrom =
+      req.query?.ageFrom !== "undefined" ? req.query?.ageFrom : undefined;
+    const gender =
+      req.query?.gender !== "undefined" ? req.query?.gender : undefined;
+    const permanentDivision =
+      req.query?.permanentDivision !== "undefined"
+        ? req.query?.permanentDivision
+        : undefined;
+    const presentDivision =
+      req.query?.presentDivision !== "undefined"
+        ? req.query?.presentDivision
+        : undefined;
+    if (ageTo && ageFrom) {
+      filter.age = { $gte: ageFrom, $lte: ageTo };
+    } else if (ageTo) {
+      filter.age = { $lte: ageTo };
+    } else if (ageFrom) {
+      filter.age = { $gte: ageFrom };
+    }
+
+    if (gender) {
+      filter.gender = gender;
+    }
+
+    if (permanentDivision) {
+      filter.permanentDivision = permanentDivision;
+    }
+    if (presentDivision) {
+      filter.presentDivision = presentDivision;
+    }
     console.log("before");
-    const result = await BioData.countDocuments();
+    const result = await BioData.countDocuments(filter);
     res.send({ result });
   } catch (error) {
     console.log(error.message);
